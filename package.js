6a60b7bb0ffe7513609cec46b5bb14bea997b1bb
Daniel Sullivan <danielalexandersullivan@gmail.com>
2016-03-09 09:20:41 -0500

    Specify package dependency versions

--- package.js
+++ package.js
@@ -8,14 +8,14 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.use('froatsnook:request')
-  api.use('peerlibrary:xml2js')
+  api.use('froatsnook:request@2.67.0')
+  api.use('peerlibrary:xml2js@0.4.4_3')
   api.addFiles('jenkins.js', 'server');
   api.export('Jenkins', 'server');
 });
 
 Package.on_test(function (api) {
   api.use(['dannysullivan:jenkins', 'tinytest', 'test-helpers'], ['server']);
-  api.use('froatsnook:request')
+  api.use('froatsnook:request@2.67.0')
   api.add_files('tests/jenkins-tests.js', ['server']);
 });
